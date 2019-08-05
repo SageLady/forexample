3ed3c67167de7d0234fba4564326be8b7ac7bdeb
Roxanne Souza <rsouza@petzent.com>
2019-08-05 14:21:33 -0700

    you're doing great

---
+++ NinjaLvl1_EX1/ExOne.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main()  {
+x := 42
+fmt.Println(x)
+y := "James Bond"
+fmt.Println(y)
+z := true
+fmt.Println(z)
+}
+
+//Hands on excercise #1
+//Using the short declaration operator, ASSIGN these VALUES to VARIABLES
+//with the IDENTIFIERS “x” and “y” and “z”
+//a. 42
+//b. “James Bond”
+//c. true
+//Now print the values stored in those variables using
+//a single print statement
+//multiple print statements
+
+//code: here’s the solution: https://play.golang.org/p/yYXnWXIQNa
+
+//Solution:
+//package main
+
+//import ( "fmt" )
+
+//func main() {
+//	x := 42
+//	y := "James Bond"
+//	z := true
+//	fmt.Println(x, y, z)
+//	fmt.Println(x)
+//	fmt.Println(y)
+//	fmt.Println(z)
+//}
