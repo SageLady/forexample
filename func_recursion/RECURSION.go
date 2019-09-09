e8552e5a728584c8cea03d112dca969c1e9cabae
Roxanne Souza <rsouza@petzent.com>
2019-09-09 15:54:46 -0700

    Mellow Monday

--- func_recursion/RECURSION.go
+++ func_recursion/RECURSION.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main()  {
 	fmt.Println(4*3*2*1)
-	n := factorial(4)
-	fmt.Println(n)
+//	n := factorial(4)
+//	fmt.Println(n)
+	v := factLoop(4)
+	fmt.Println("\n")
+	fmt.Println(v)
 
 
 }  // END main
@@ -18,11 +21,23 @@ func factorial(n int) int {
 }  // END factorial
 
 func factLoop(n int) int {
-	for i == 0; i <= n; i++ {
-		
-
+	var factorial = 1
+	for n != 0 {
+		factorial *= n
+		fmt.Printf("\nn equals %v", n)
+		fmt.Printf("\nfactorial equals  %v", factorial)
+		n--
 	}
-
+	return factorial
 }
 
 // 4 * 3 * 2 * 1 * 1
+
+//Solution in class:
+//func loopFact(n int) int {
+//	total := 1
+//	for ; n > 0; n-- {
+//		total *= n
+//	}
+//	return total
+//}
